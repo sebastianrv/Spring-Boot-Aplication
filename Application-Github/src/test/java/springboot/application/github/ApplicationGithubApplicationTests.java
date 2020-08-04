257866d220b9d42df71906398dc1e7b4a544abaf
sebastian <romero_villa-sebastian@hotmail.com>
2020-08-04 13:42:34 -0500

    Commit inicial con Spring Boot

---
+++ Application-Github/src/test/java/springboot/application/github/ApplicationGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.application.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
