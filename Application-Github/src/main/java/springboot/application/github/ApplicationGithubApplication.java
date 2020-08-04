4ef266dc4fcfe7a26b84ab1001bf50a1d896a6e1
sebastian <romero_villa-sebastian@hotmail.com>
2020-08-04 13:42:34 -0500

    Commit inicial con Spring Boot. Close #1

---
+++ Application-Github/src/main/java/springboot/application/github/ApplicationGithubApplication.java
@@ -0,0 +1,13 @@
+package springboot.application.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationGithubApplication.class, args);
+	}
+
+}
